4fa466372d24cee1a264713c45026861f81b4ba1
Chris McFarlen <cmcfarlen@apple.com>
2025-06-07 18:25:59 -0500

    SshAgent auth delegate connection state

--- Sources/NIOSSH/SshAgent/SshAgentUserAuthenticationDelegate.swift
+++ Sources/NIOSSH/SshAgent/SshAgentUserAuthenticationDelegate.swift
@@ -12,24 +12,31 @@
 //
 //===----------------------------------------------------------------------===//
 
-@preconcurrency import Crypto
 import Foundation
+import NIOConcurrencyHelpers
 import NIOCore
-import NIOFoundationCompat
 
 public final class SshAgentUserAuthenticationDelegate<Bootstrap: NIOClientTCPBootstrapProtocol> {
+
+    enum AgentConnectState {
+        case notConnected
+        case connecting([EventLoopPromise<Channel>])
+        case connected(Channel)
+        case failed(any Error)
+    }
+
+    let agentState: NIOLockedValueBox<AgentConnectState>
     let socketPath: String
-    var channel: Channel? = nil
-    var pendingSignatures: [ByteBuffer: EventLoopPromise<NIOSSHUserAuthenticationOutcome>] = [:]
-    let bootstrapper: (any EventLoop) throws -> Bootstrap
+    let bootstrapper: @Sendable (any EventLoop) throws -> Bootstrap
     let username: String
 
     // TODO(cmcfarlen): Add config to specify a key to use by label
-    public init(username: String, _ bootstrapper: @escaping (any EventLoop) throws -> Bootstrap) {
+    public init(username: String, _ bootstrapper: @Sendable @escaping (any EventLoop) throws -> Bootstrap) {
         // TODO(cmcfarlen): Don't be so fatal
         guard let path = Self.environmentAgent else {
             fatalError("Failed to find ssh-agent socket from environment")
         }
+        self.agentState = .init(.notConnected)
         self.socketPath = path
         self.bootstrapper = bootstrapper
         self.username = username
@@ -43,6 +50,8 @@ public final class SshAgentUserAuthenticationDelegate<Bootstrap: NIOClientTCPBoo
 
     }
 }
+
+extension SshAgentUserAuthenticationDelegate: Sendable {}
 
 extension NIOSSHPublicKey {
     func rawBytesToSignature(raw: ByteBuffer) throws -> NIOSSHSignature? {
@@ -63,8 +72,116 @@ extension SshAgentUserAuthenticationDelegate: NIOSSHClientUserAuthenticationDele
         authInfo: any NIOSSHClientUserAuthenticationInfo,
         nextChallengePromise: EventLoopPromise<NIOSSHUserAuthenticationOffer?>
     ) {
+        let username = self.username
+        getAgentChannel(eventLoop: authInfo.eventLoop)
+            .whenComplete { result in
+                switch result {
+                case .failure(let err):
+                    nextChallengePromise.fail(err)
+                case .success(let channel):
+                    SshAgentUserAuthenticationDelegate.makeRequest(channel: channel, request: .requestIdentities)
+                        .whenComplete { result in
+                            switch result {
+                            case .failure(let err):
+                                nextChallengePromise.fail(err)
+                            case .success(let resp):
+                                switch resp {
+                                case .identities(let ids):
+                                    print("Got \(ids.count) ids")
+                                    let pubkey = ids[0].publicKey
+                                    let signBytes = authInfo.generateSignableAuthPayload(
+                                        username: username,
+                                        forKey: pubkey
+                                    )
+                                    _ = channel.eventLoop.submit {
+                                        SshAgentUserAuthenticationDelegate.makeRequest(
+                                            channel: channel,
+                                            request: .signRequest(
+                                                keyBlob: Array(ids[0].key.readableBytesView),
+                                                data: Array(signBytes.readableBytesView),
+                                                flags: 0
+                                            )
+                                        )
+                                        .whenComplete { result in
+                                            switch result {
+                                            case .failure(let err):
+                                                nextChallengePromise.fail(err)
+                                            case .success(let resp):
+                                                switch resp {
+                                                case .signResponse(let signature):
+                                                    nextChallengePromise.succeed(
+                                                        .init(
+                                                            username: username,
+                                                            serviceName: "",
+                                                            offer: .agentSigned(
+                                                                pubkey,
+                                                                try? pubkey.rawBytesToSignature(raw: signature)
+                                                            )
+                                                        )
+                                                    )
+                                                default:
+                                                    nextChallengePromise.fail(
+                                                        NIOSSHAgentError.unexpectedResponse(
+                                                            reason:
+                                                                "Unexpected response for request signature: \(result)"
+                                                        )
+                                                    )
+                                                }
+                                            }
+                                        }
+                                    }
+                                default:
+                                    nextChallengePromise.fail(
+                                        NIOSSHAgentError.unexpectedResponse(
+                                            reason: "Unexpected response for request ids: \(resp)"
+                                        )
+                                    )
+                                }
+                            }
+
+                        }
+                }
+
+            }
+    }
+
+    public func nextAuthenticationType(
+        availableMethods: NIOSSHAvailableUserAuthenticationMethods,
+        nextChallengePromise: EventLoopPromise<NIOSSHUserAuthenticationOffer?>
+    ) {
+        fatalError("This overload not supported")
+    }
+}
+
+extension SshAgentUserAuthenticationDelegate {
+    private func getAgentChannel(eventLoop: EventLoop) -> EventLoopFuture<Channel> {
+        let promise = eventLoop.makePromise(of: Channel.self)
+        var doConnect = false
+        agentState.withLockedValue { state in
+            switch state {
+            case .notConnected:
+                doConnect = true
+                state = .connecting([promise])
+            case .connecting(var waiters):
+                waiters.append(promise)
+                state = .connecting(waiters)
+            case .connected(let channel):
+                promise.succeed(channel)
+            case .failed(let err):
+                promise.fail(err)
+            }
+        }
+
+        if doConnect {
+            startAgentConnect(eventLoop: eventLoop)
+        }
+
+        return promise.futureResult
+    }
+
+    private func startAgentConnect(eventLoop: EventLoop) {
         do {
-            let bootstrap = try bootstrapper(authInfo.eventLoop)
+            let bootstrap = try bootstrapper(eventLoop)
                 .channelOption(ChannelOptions.socketOption(.so_reuseaddr), value: 1)
                 .channelInitializer { channel in
                     // The pipeline processes data and events. Add your handler here.
@@ -78,88 +195,31 @@ extension SshAgentUserAuthenticationDelegate: NIOSSHClientUserAuthenticationDele
                 }
 
             print("Connecting to \(socketPath)")
-            let username = self.username
             bootstrap.connect(unixDomainSocketPath: socketPath)
-                .whenComplete { result in
-                    switch result {
-                    case .failure(let err):
-                        nextChallengePromise.fail(err)
-                    case .success(let channel):
-                        SshAgentUserAuthenticationDelegate.makeRequest(channel: channel, request: .requestIdentities)
-                            .whenComplete { result in
-                                switch result {
-                                case .failure(let err):
-                                    nextChallengePromise.fail(err)
-                                case .success(let resp):
-                                    switch resp {
-                                    case .identities(let ids):
-                                        print("Got \(ids.count) ids")
-                                        let pubkey = ids[0].publicKey
-                                        let signBytes = authInfo.generateSignableAuthPayload(
-                                            username: username,
-                                            forKey: pubkey
-                                        )
-                                        _ = channel.eventLoop.submit {
-                                            SshAgentUserAuthenticationDelegate.makeRequest(
-                                                channel: channel,
-                                                request: .signRequest(
-                                                    keyBlob: Array(ids[0].key.readableBytesView),
-                                                    data: Array(signBytes.readableBytesView),
-                                                    flags: 0
-                                                )
-                                            )
-                                            .whenComplete { result in
-                                                switch result {
-                                                case .failure(let err):
-                                                    nextChallengePromise.fail(err)
-                                                case .success(let resp):
-                                                    switch resp {
-                                                    case .signResponse(let signature):
-                                                        nextChallengePromise.succeed(
-                                                            .init(
-                                                                username: username,
-                                                                serviceName: "",
-                                                                offer: .agentSigned(
-                                                                    pubkey,
-                                                                    try? pubkey.rawBytesToSignature(raw: signature)
-                                                                )
-                                                            )
-                                                        )
-                                                    default:
-                                                        nextChallengePromise.fail(
-                                                            NIOSSHAgentError.unexpectedResponse(
-                                                                reason:
-                                                                    "Unexpected response for request signature: \(result)"
-                                                            )
-                                                        )
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    default:
-                                        nextChallengePromise.fail(
-                                            NIOSSHAgentError.unexpectedResponse(
-                                                reason: "Unexpected response for request ids: \(resp)"
-                                            )
-                                        )
-                                    }
+                .whenComplete { [weak self] result in
+                    self?.agentState.withLockedValue { state in
+                        switch state {
+                        case .connecting(let waiters):
+                            switch result {
+                            case .success(let channel):
+                                for w in waiters {
+                                    w.succeed(channel)
                                 }
-
+                                state = .connected(channel)
+                            case .failure(let err):
+                                for w in waiters {
+                                    w.fail(err)
+                                }
+                                state = .failed(err)
                             }
+                        default:
+                            fatalError("Unexpected SshAgent connect state.  Expected .connecting, got \(state)")
+                        }
                     }
                 }
-
         } catch {
-            nextChallengePromise.fail(error)
+            fatalError("Failed bootstrapping ssh agent connection \(error)")
         }
-
-    }
-
-    public func nextAuthenticationType(
-        availableMethods: NIOSSHAvailableUserAuthenticationMethods,
-        nextChallengePromise: EventLoopPromise<NIOSSHUserAuthenticationOffer?>
-    ) {
-        fatalError("This overload not supported")
     }
 }
 
